640eae28a293356c48e6a44e4d14900606ab1185
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-07-20 15:34:31 -0300

    Terminei o layout basico. Começar a fazer o map e interações

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
+import GameScreen from './GameScreen'
 import HomeScreen from './HomeScreen'
 
 export default function App() {
     return (
         <>
             <HomeScreen />
-            <div className='game-screen'>
-                <div>
-                    <img src="../assets/img/logo.png" />
-                    <h1>ZapRecall</h1>
-                </div>
-                <div>0/4 CONCLUÍDOS</div>
-            </div>
+            <GameScreen />
         </>
     )
 }
